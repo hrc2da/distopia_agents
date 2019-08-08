77e4c48cd00036e927fce724887517c1e7fe4b71
mattvlaw <mattvlaw@gmail.com>
2019-08-08 17:57:25 -0400

    added data dumping on run and handling for case where there's no legal states (untested)

--- agents/bfagent.py
+++ agents/bfagent.py
@@ -153,14 +153,19 @@ class GreedyAgent(DistopiaAgent):
             return np.dot(self.reward_weights, metrics)
 
     def run(self, n_steps, initial=None, eps=0.9, eps_decay=0.9):
-        '''
+        '''runs for n_steps and returns traces of designs and metrics
         '''
         self.reset(initial)
+        all_designs = []
+        all_metrics = []
         for i in range(n_steps):
             neighborhood = self.get_neighborhood(1)
             metrics = [self.get_metrics(n) for n in neighborhood]
             rewards = [self.get_reward(m) for m in metrics]
             best_idx = np.argmax(rewards)
+            # if there's no legal states then just reset
+            if rewards[best_idx] == float("-inf"):
+                self.reset(initial)
             if np.random.rand() > eps:
                 # mask out the legal options
                 legal_mask = np.array([1 if r > float("-inf") else 0 for r in rewards])
@@ -169,6 +174,9 @@ class GreedyAgent(DistopiaAgent):
                 best_idx = np.random.choice(rewards, legal_mask)
             # TODO: need to update occupied when changing state
             self.state = neighborhood[best_idx]
+            all_design.append(self.state)
+            all_metrics.append(metrics[best_idx])
+        return all_designs, all_metrics
 
 
 if __name__ == '__main__':
