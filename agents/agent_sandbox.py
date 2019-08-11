0268b046a2c5d6657725b51da13b440e18e6ff62
Amrit <ak2244@cornell.edu>
2019-08-11 13:22:15 -0700

    new task parsing, multiple tasks

--- agents/agent_sandbox.py
+++ agents/agent_sandbox.py
@@ -6,30 +6,64 @@ import sys
 import pickle as pkl
 from scipy.stats import multivariate_normal
 
-emissions_file = '/home/dev/research/distopia/logs/conditional_one_hots.pkl'
+emissions_file = 'resources/conditional_one_hots.pkl'
 
 with open(emissions_file, 'rb') as em_file:
-	emissions_probs = pkl.load(em_file)
+    emissions_probs = pkl.load(em_file)
 
-normalization_file = '/home/dev/research/distopia/logs/normalization.pkl'
+normalization_file = 'resources/normalization.pkl'
 
-with open(normalization_file,'rb') as z_file:
-	means, stds = pkl.load(z_file)
-	means = np.array(means)
-	stds = np.array(stds)
+with open(normalization_file, 'rb') as z_file:
+    means, stds = pkl.load(z_file)
+    means = np.array(means)
+    stds = np.array(stds)
 
-def transition_fn(x,y,z):
-	return 1/14
-def observation_fn(observation,task):
-	means,cov = emissions_probs[str(list(task))]
-	return multivariate_normal.pdf(observation,mean=means,cov=cov)
+
+def transition_fn(x, y, z):
+    return 1/14
+
+
+def observation_fn(observation, task):
+    means, cov = emissions_probs[str(list(task))]
+    return multivariate_normal.pdf(observation, mean=means, cov=cov)
+
+
 def standardize(metric_arr):
-	return (metric_arr - means) / stds
-	
+    return (metric_arr - means) / stds
+
+
+def parse_task(task_desc):
+    tasks = []
+    if "|" in task_desc:
+        split_task_descs = task_desc.split("|")
+    else:
+        split_task_descs = [task_desc]
+    for task_desc in split_task_descs:
+        # get task
+        task_str = task_desc[task_desc.index("(")+1:task_desc.index(")")]
+        task = np.array([int(i) for i in task_str.split(',')])
+        # get string
+        if "[" in task_desc and "]" in task_desc:
+            steps = int(task_desc[task_desc.index("[")+1:task_desc.index("]")])
+        else:
+            steps = 35
+        tasks.append((task, steps))
+    return tasks
+
+
+def stringify_task(task):
+    s = "["
+    for e in task:
+        s += str(e) + ","
+    s = s[:-1] + "]"
+    return s
+
+
 metrics = ['population', 'pvi', 'compactness', 'projected_votes', 'race', 'income', 'area']
 five_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
 six_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
-seven_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
+seven_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1],
+                                      [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
 
 tasks = []
 
@@ -41,23 +75,32 @@ for i in range(len(metrics)):
     tasks.append(task_up)
     tasks.append(task_down)
 
-
-n_steps = int(sys.argv[1])
+all_tasks = []
 with open('task.txt', 'r') as f:
-	task = f.readline()
-task = np.array([int(i) for i in task.split(',')])
+    for task_desc in f:
+        given_tasks = parse_task(task_desc)
+        all_tasks.append(given_tasks)
+
 gagent = GreedyAgent()
 gagent.set_metrics(metrics)
-gagent.set_task(task)
-all_designs, all_metrics = gagent.run(n_steps)
 
-bfilter = BayesFilter(tasks, transition_fn, observation_fn)
+for task_trajectory in all_tasks:
+    task_path = ""
+    all_designs = []
+    all_metrics = []
+    for task, steps in task_trajectory:
+        task_path += stringify_task(task) + "(" + str(steps) + ")>"
+        gagent.set_task(task)
+        designs, metrics = gagent.run(steps)
+        all_designs += designs
+        all_metrics += metrics
 
-with open('belief_file.txt', 'w') as f:
-	for metric in all_metrics:
-		action = None
-		#b_star = bfilter.prediction_step(action)
-		b = bfilter.observation_step(standardize(metric))
-		#f.write(str("Pred: " + str(b_star)))
-		f.write(str("Obs: " + str(b)))
-		f.write('\n')
+    bfilter = BayesFilter(tasks, transition_fn, observation_fn)
+    with open('results/'+task_path[:-1]+'_belief_file.txt', 'w') as f:
+        for metric in all_metrics:
+            action = None
+            #b_star = bfilter.prediction_step(action)
+            b = bfilter.observation_step(standardize(metric))
+            #f.write(str("Pred: " + str(b_star)))
+            f.write(str("Obs: " + str(b)))
+            f.write('\n')
