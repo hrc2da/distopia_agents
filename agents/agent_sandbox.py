836fa72b6d64f5506ed5dc9d7dce9a6b29c88776
mattvlaw <mattvlaw@gmail.com>
2019-08-11 13:52:27 -0400

    adding some sandbox changes

--- agents/agent_sandbox.py
+++ agents/agent_sandbox.py
@@ -3,15 +3,46 @@ from bfagent import GreedyAgent
 import itertools
 import numpy as np
 import sys
+import pickle as pkl
+from scipy.stats import multivariate_normal
+
+emissions_file = '/home/dev/research/distopia/logs/conditional_one_hots.pkl'
+
+with open(emissions_file, 'rb') as em_file:
+	emissions_probs = pkl.load(em_file)
+
+normalization_file = '/home/dev/research/distopia/logs/normalization.pkl'
+
+with open(normalization_file,'rb') as z_file:
+	means, stds = pkl.load(z_file)
+	means = np.array(means)
+	stds = np.array(stds)
+
 def transition_fn(x,y,z):
-	return 0.5
-def observation_fn(x,y):
-	return 0.5
+	return 1/14
+def observation_fn(observation,task):
+	means,cov = emissions_probs[str(list(task))]
+	return multivariate_normal.pdf(observation,mean=means,cov=cov)
+def standardize(metric_arr):
+	return (metric_arr - means) / stds
+	
 metrics = ['population', 'pvi', 'compactness', 'projected_votes', 'race', 'income', 'area']
 five_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
 six_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
 seven_states = list(itertools.product([-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1], [-1, 0, 1]))
-n_steps = sys.argv[1]
+
+tasks = []
+
+for i in range(len(metrics)):
+    task_up = np.zeros(len(metrics))
+    task_up[i] = 1
+    task_down = np.zeros(len(metrics))
+    task_down[i] = -1
+    tasks.append(task_up)
+    tasks.append(task_down)
+
+
+n_steps = int(sys.argv[1])
 with open('task.txt', 'r') as f:
 	task = f.readline()
 task = np.array([int(i) for i in task.split(',')])
@@ -20,13 +51,13 @@ gagent.set_metrics(metrics)
 gagent.set_task(task)
 all_designs, all_metrics = gagent.run(n_steps)
 
-bfilter = BayesFilter(seven_states, transition_fn, observation_fn)
+bfilter = BayesFilter(tasks, transition_fn, observation_fn)
 
 with open('belief_file.txt', 'w') as f:
 	for metric in all_metrics:
 		action = None
-		b_star = bfilter.prediction_step(action)
-		b = bfilter.observation_step(metric)
-		f.write(str("Pred: " + str(b_star)))
+		#b_star = bfilter.prediction_step(action)
+		b = bfilter.observation_step(standardize(metric))
+		#f.write(str("Pred: " + str(b_star)))
 		f.write(str("Obs: " + str(b)))
 		f.write('\n')
--- agents/bfilter.py
+++ agents/bfilter.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pandas import *
+#from pandas import *
 a = np.matrix([[5,3,0,0,-4,-1],
               [3,1,0,0,-2,-1],
               [4,4,1,1,-2,-4],
