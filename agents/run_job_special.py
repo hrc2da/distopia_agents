c0a95e3f8e05d03d17596b86103be5df2bf52c49
mattvlaw <mattvlaw@gmail.com>
2019-08-17 04:04:08 +0000

    typo

--- agents/run_job_special.py
+++ agents/run_job_special.py
@@ -60,7 +60,7 @@ if __name__ == '__main__':
 	status_queue = Queue()
 	total_steps = np.sum([tup[1] for tup in task_tuples])
 	def progress_monitor():
-		for i in tqdm(range(total_steps):
+		for i in tqdm(range(total_steps)):
 			status_queue.get()
 
 	def process_task(i, task_tuple):
