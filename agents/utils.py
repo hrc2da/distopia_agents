029b88ebefff0943a265ab07f535a9be7c2be763
mattvlaw <mattvlaw@gmail.com>
2019-08-23 15:45:37 -0400

    added str2arr

--- agents/utils.py
+++ agents/utils.py
@@ -1,12 +1,26 @@
-def hierarchical_sort(lst,direction='lr',order='descending'):
+import numpy as np
+
+
+def hierarchical_sort(lst, direction='lr', order='descending'):
     assert len(lst) > 0
     to_sort = lst[:]
     sample = to_sort[0]
     sort_order = -1 if order == 'descending' else 1
-    if direction == 'lr':
+    if direction == 'rl':
         for i in range(len(sample)):
-            to_sort.sort(key = lambda x : sort_order * x[i])
-    elif direction == 'rl':
-        for j in range(len(sample),0,-1):
-            to_sort.sort(key = lambda x : sort_order * x[i])
+            to_sort.sort(key=lambda x: sort_order * x[i])
+    elif direction == 'lr':
+        for j in range(len(sample)-1, -1, -1):
+            to_sort.sort(key=lambda x: sort_order * x[j])
     return to_sort
+
+
+def str2arr(string: str):
+    ''' convert a stringified np array back to the array
+    :param string: the stringified np array
+    :return: a np array
+    '''
+    assert type(string) == str
+    assert string[0] == '['
+    assert string[-1] == ']'
+    return np.array(string[1:-1].split(), dtype=float)
